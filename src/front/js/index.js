33ebdd567debbe64abc0131363dfd52bbb6cc8d9
Ehiber Graterol <56272634+ehiber@users.noreply.github.com>
2023-11-02 16:07:20 +0000

    :sparkles: update React version and Node engine version

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,6 +1,6 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
@@ -9,4 +9,7 @@ import "../styles/index.css";
 import Layout from "./layout";
 
 //render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+const domNode = document.querySelector("#app");
+const root = createRoot(domNode);
+
+root.render(<Layout />)
